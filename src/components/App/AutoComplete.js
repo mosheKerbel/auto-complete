2adaaa2c324ef4ab9406fc610eb60990db902432
moshek <moshek@wix.com>
2019-10-25 23:49:18 +0300

    support basic functionality

--- src/components/App/AutoComplete.js
+++ src/components/App/AutoComplete.js
@@ -1,33 +1,152 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./AutoComplete.module.css";
+import React from 'react';
+import classes from 'classnames';
+import withClickOutside from 'react-click-outside';
+
+import PropTypes from 'prop-types';
+import styles from './AutoComplete.module.css';
 
 class AutoComplete extends React.Component {
+  state = {
+    opened: false,
+    selectedOption: null,
+    hoveredOption: null,
+    value: '',
+  };
 
   static propTypes = {
-    options: PropTypes.array.isRequired
+    options: PropTypes.array.isRequired,
+    onChange: PropTypes.func,
   };
 
   static defaultProps = {
     options: [],
+    onChange: () => {},
+  };
+
+  _onInputValueChange = e => {
+    const {onChange} = this.props;
+    const {
+      target: { value },
+    } = e;
+    this.setState(() => {
+      return {
+        value: value,
+      };
+    }, onChange(value));
+  };
+
+  _onOptionClick = option => {
+    this._closeOptions();
+    this.setState(() => {
+      return {
+        selectedOption: option,
+        value: option.displayValue,
+      };
+    });
+  };
+
+  _onOptionHover = option => {
+    this.setState(() => {
+      return {
+        hoveredOption: option,
+      };
+    });
+  };
+
+  _onOptionOut = () => {
+    this.setState(() => {
+      return {
+        hoveredOption: null,
+      };
+    });
+  };
+
+  _openOptions = () => {
+    this.setState(() => {
+      return {
+        opened: true,
+      };
+    });
+  };
+
+  _closeOptions = () => {
+    this.setState(() => {
+      return {
+        opened: false,
+      };
+    });
+  };
+
+  handleClickOutside() {
+    // used by `withClickOutside` HOC
+    this._closeOptions();
+  }
+
+  _isOptionHovered = option => {
+    const { hoveredOption } = this.state;
+    return hoveredOption && hoveredOption.value === option.value;
+  };
+
+  _isOptionSelected = option => {
+    const { selectedOption } = this.state;
+    return selectedOption && selectedOption.value === option.value;
   };
 
   _renderOption(option, idx) {
-    return <div key={`option${idx}`} className={styles.option}>{option}</div>;
+    const isOptionHovered = this._isOptionHovered(option);
+    const isOptionSelected = this._isOptionSelected(option);
+    return (
+      <div
+        key={`option${idx}`}
+        className={classes(
+          styles.option,
+          isOptionSelected ? styles.option_selected : {},
+          !isOptionSelected && isOptionHovered ? styles.option_hover : {},
+        )}
+        onClick={() => this._onOptionClick(option)}
+        onMouseOver={() => this._onOptionHover(option)}
+        onMouseOut={() => this._onOptionOut()}
+      >
+        {option.displayValue}
+      </div>
+    );
   }
 
+  _renderOptions = () => {
+    const { options } = this.props;
+    const { opened } = this.state;
+    return (
+      <div
+        className={classes(
+          styles.options,
+          opened ? styles.options_opened : styles.options_closed,
+        )}
+      >
+        {options.map((option, idx) => this._renderOption(option, idx))}
+      </div>
+    );
+  };
+
+  _renderInput = () => {
+    const { value } = this.state;
+    return (
+      <input
+        onClick={this._openOptions}
+        className={styles.input}
+        value={value}
+        onChange={this._onInputValueChange}
+      />
+    );
+  };
+
   render() {
-    const {options} = this.props;
     return (
       <div className={styles.root}>
-        <input className={styles.input}/>
-        <div className={styles.options}>
-          {options.map((option, idx) => this._renderOption(option, idx))}
-        </div>
+        {this._renderInput()}
+        {this._renderOptions()}
       </div>
     );
   }
 }
 
-
-export default AutoComplete;
+export default withClickOutside(AutoComplete);
