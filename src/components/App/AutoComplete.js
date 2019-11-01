4d7b17d17fb0e37967156c59fd161ac38b5f8d4a
moshek <moshek@wix.com>
2019-11-01 12:59:56 +0200

    support keys

--- src/components/App/AutoComplete.js
+++ src/components/App/AutoComplete.js
@@ -24,7 +24,7 @@ class AutoComplete extends React.Component {
   };
 
   _onInputValueChange = e => {
-    const {onChange} = this.props;
+    const { onChange } = this.props;
     const {
       target: { value },
     } = e;
@@ -45,7 +45,7 @@ class AutoComplete extends React.Component {
     });
   };
 
-  _onOptionHover = option => {
+  _hoverOption = option => {
     this.setState(() => {
       return {
         hoveredOption: option,
@@ -92,6 +92,89 @@ class AutoComplete extends React.Component {
     return selectedOption && selectedOption.value === option.value;
   };
 
+  _selectOption = (e) => {
+    const { hoveredOption } = this.state;
+    if (!hoveredOption) {
+      return;
+    }
+    this.setState({
+      selectedOption: hoveredOption,
+      hoveredOption: null,
+      value: hoveredOption.displayValue,
+    });
+    this._closeOptions();
+    e.preventDefault();
+  };
+
+  _onKeyDown = e => {
+    // tab || enter
+    if (e.keyCode === 9 || e.keyCode === 13) {
+      this._selectOption(e);
+    } else if (e.keyCode === 27) {
+      this._closeOptions();
+    } else if (e.keyCode === 38) {
+      this._openOptions();
+      this._hoverPrevOption();
+    } else if (e.keyCode === 40) {
+      this._openOptions();
+      this._hoverNextOption();
+    }
+  };
+
+  _getOptionIndex = (hoveredOption, options) => {
+    for (let i = 0; i < options.length; i++) {
+      const o = options[i];
+      if (o === hoveredOption) {
+        return i;
+      }
+    }
+
+    return -1;
+  };
+
+  _hoverPrevOption = () => {
+    const i = 0;
+    const { hoveredOption } = this.state;
+    const { options } = this.props;
+
+    // no option -> do nothing
+    if (!hoveredOption) {
+      return;
+    }
+
+    // first option is hovered -> hover the last one
+    const optionIndex = this._getOptionIndex(hoveredOption, options);
+    if (hoveredOption === options[0]) {
+      const lastOptionIndex = options.length - 1;
+      this._hoverOption(options[lastOptionIndex]);
+      return;
+    }
+
+    const nextOption = options[optionIndex - 1];
+    this._hoverOption(nextOption);
+  };
+
+  _hoverNextOption = () => {
+    const { hoveredOption } = this.state;
+    const { options } = this.props;
+
+    // no option -> hover the first one
+    if (!hoveredOption) {
+      this._hoverOption(options[0]);
+      return;
+    }
+
+    // last option is hovered -> hover the first one
+    const optionIndex = this._getOptionIndex(hoveredOption, options);
+    if (optionIndex === options.length - 1) {
+      this._hoverOption(options[0]);
+      return;
+    }
+
+    const nextOption = options[optionIndex + 1];
+    this._hoverOption(nextOption);
+  };
+
   _renderOption(option, idx) {
     const isOptionHovered = this._isOptionHovered(option);
     const isOptionSelected = this._isOptionSelected(option);
@@ -104,7 +187,7 @@ class AutoComplete extends React.Component {
           !isOptionSelected && isOptionHovered ? styles.option_hover : {},
         )}
         onClick={() => this._onOptionClick(option)}
-        onMouseOver={() => this._onOptionHover(option)}
+        onMouseOver={() => this._hoverOption(option)}
         onMouseOut={() => this._onOptionOut()}
       >
         {option.displayValue}
@@ -135,6 +218,7 @@ class AutoComplete extends React.Component {
         className={styles.input}
         value={value}
         onChange={this._onInputValueChange}
+        onKeyDown={this._onKeyDown}
       />
     );
   };
